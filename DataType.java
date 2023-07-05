0b851f0cc75e4277041dd0f49df5faad67d195ff
“Aashish <aashishkumawat930@gmail.com>
2023-07-05 19:39:03 +0530

    Added : simple java program

---
+++ DataType.java
@@ -0,0 +1,24 @@
+package com.bl.day7;
+
+public class DataType {
+
+        public static void main(String[] args){
+            byte a=1;
+            short b=12;
+            int c=123;
+            long d=123456789;
+            float f=12.25f;
+            double g=521456.2563;
+            char h='G';
+            boolean flag=true;
+            System.out.println("byte  " + a);
+            System.out.println("short  " + b);
+            System.out.println("int  " + c);
+            System.out.println("long  " + d);
+            System.out.println("float  " + f);
+            System.out.println("double  " + g);
+            System.out.println("char  " + h);
+            System.out.println("boolean " + flag);
+
+    }
+}
---
+++ StringEqual.java
@@ -0,0 +1,14 @@
+package com.bl.day7;
+public class StringEqual {
+    public static void main(String[] args){
+        java.lang.String name = "Aashish";
+        java.lang.String name1 = "Aashish";
+        java.lang.String name2 = "Bhani";
+            if(name == name1){
+                System.out.println("String are equal");
+            }
+            else{
+                System.out.println("String are not equal");
+            }
+    }
+}
